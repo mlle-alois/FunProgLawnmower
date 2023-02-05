7021585cf7fe2b0a3cea006d8a90de0e3be0d499
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-02-05 16:59:31 +0100

    [END JSON FILE]

--- src/main/scala/progfun/Main.scala
+++ src/main/scala/progfun/Main.scala
@@ -2,6 +2,7 @@ package progfun
 
 import better.files.File
 import com.typesafe.config.{Config, ConfigFactory}
+import play.api.libs.json.Json
 import progfun.input.InputParser
 import progfun.output.JsonOutput
 import progfun.output.JsonOutput.lawnmowersSerializer
@@ -15,35 +16,26 @@ object Main extends App {
   private val inputParser = new InputParser
   private val input = inputParser.parse(inputFileName)
 
-  //val lawnmowers = collection.mutable.Map[Lawnmower, Position]()
-
   val lawnmowers = for (lawnmower <- input.lawnmowers) yield lawnmower.copy(finalPosition = lawnmower.followInstructions(input.grass))
   println(lawnmowers(0).finalPosition.coordinate.y)
 
   private val outputTypeChoice = conf.getString("appplication.output-type-choice")
   outputTypeChoice match {
     case "json" => {
+      //TODO déplacer ça dans une classe + cas de test
       val ouputFileName = conf.getString("appplication.output-json-file")
       val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
       val f = File(ouputFileName)
 
-      f.createIfNotExists().appendText(jsonValue.toString())
+      f.createIfNotExists().overwrite(Json.prettyPrint(jsonValue))
       println("Successfully written to JSON file")
     }
     case "yaml" => {
-      val ouputFileName = conf.getString("appplication.output-json-file")
-      val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
-      val f = File(ouputFileName)
 
-      f.createIfNotExists().overwrite(jsonValue.toString)
       println("Successfully written to YAML file")
     }
     case "csv" => {
-      val ouputFileName = conf.getString("appplication.output-json-file")
-      val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
-      val f = File(ouputFileName)
 
-      f.createIfNotExists().overwrite(jsonValue.toString)
       println("Successfully written to CSV file")
     }
     case _ => println("Invalid output type choice")
