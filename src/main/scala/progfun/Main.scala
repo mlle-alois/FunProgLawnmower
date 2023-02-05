a9e876f9e0dac44a1d8e7d1b80d9a59d50a2a16d
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-02-05 16:49:38 +0100

    [START SAVING FILE]

--- src/main/scala/progfun/Main.scala
+++ src/main/scala/progfun/Main.scala
@@ -1,16 +1,19 @@
 package progfun
 
-import progfun.input.InputParser
+import better.files.File
 import com.typesafe.config.{Config, ConfigFactory}
+import progfun.input.InputParser
+import progfun.output.JsonOutput
+import progfun.output.JsonOutput.lawnmowersSerializer
 
 object Main extends App {
 
   val conf: Config = ConfigFactory.load()
 
-  private val inputFile = conf.getString("appplication.input-file")
+  private val inputFileName = conf.getString("appplication.input-file")
 
   private val inputParser = new InputParser
-  private val input = inputParser.parse(inputFile)
+  private val input = inputParser.parse(inputFileName)
 
   //val lawnmowers = collection.mutable.Map[Lawnmower, Position]()
 
@@ -20,11 +23,28 @@ object Main extends App {
   private val outputTypeChoice = conf.getString("appplication.output-type-choice")
   outputTypeChoice match {
     case "json" => {
-      // val jsonOutput = new JsonOutput(input.grass, lawnmowers.toMap)
+      val ouputFileName = conf.getString("appplication.output-json-file")
+      val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
+      val f = File(ouputFileName)
+
+      f.createIfNotExists().appendText(jsonValue.toString())
+      println("Successfully written to JSON file")
     }
     case "yaml" => {
+      val ouputFileName = conf.getString("appplication.output-json-file")
+      val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
+      val f = File(ouputFileName)
+
+      f.createIfNotExists().overwrite(jsonValue.toString)
+      println("Successfully written to YAML file")
     }
     case "csv" => {
+      val ouputFileName = conf.getString("appplication.output-json-file")
+      val jsonValue = JsonOutput.serialize(input.grass, lawnmowers)
+      val f = File(ouputFileName)
+
+      f.createIfNotExists().overwrite(jsonValue.toString)
+      println("Successfully written to CSV file")
     }
     case _ => println("Invalid output type choice")
   }
