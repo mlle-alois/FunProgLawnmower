93b0a5034cb13221c96d277e15dd0ddf37ae2c2e
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2023-02-06 21:36:54 +0100

    [REFACTO]

--- src/main/scala/progfun/serializer/CSVSerializer.scala
+++ src/main/scala/progfun/serializer/CSVSerializer.scala
@@ -12,12 +12,30 @@ object CSVSerializer {
   implicit val lawnmowersSerializer
   : OutputSerializer[Grass, List[Lawnmower], String] = {
     (grass: Grass, lawnmowers: List[Lawnmower]) => {
-      List("numéro;", "taille_x;", "taille_y;", "début_x;", "début_y;", "début_direction;", "fin_x;", "fin_y;", "fin_direction;", "instructions;", "\n", lawnmowers.zipWithIndex.map {
-        case (lawnmower, index) =>
-          List((index + 1).toString, grass.width.toString, grass.height.toString, lawnmower.currentPosition.coordinate.x.toString, lawnmower.currentPosition.coordinate.y.toString,
-            lawnmower.currentPosition.orientation, lawnmower.finalPosition.coordinate.x.toString, lawnmower.finalPosition.coordinate.y.toString, lawnmower.finalPosition.orientation,
-            lawnmower.instructions.value, "\n").mkString(";")
-      }.mkString("")).mkString("")
+      List("numéro;",
+        "taille_x;",
+        "taille_y;",
+        "début_x;",
+        "début_y;",
+        "début_direction;",
+        "fin_x;",
+        "fin_y;",
+        "fin_direction;",
+        "instructions\n",
+        lawnmowers.zipWithIndex.map {
+          case (lawnmower, index) =>
+            List((index + 1).toString,
+                  grass.width.toString,
+                  grass.height.toString,
+                  lawnmower.currentPosition.coordinate.x.toString,
+                  lawnmower.currentPosition.coordinate.y.toString,
+                  lawnmower.currentPosition.orientation,
+                  lawnmower.finalPosition.coordinate.x.toString,
+                  lawnmower.finalPosition.coordinate.y.toString,
+                  lawnmower.finalPosition.orientation,
+                  lawnmower.instructions.value)
+              .mkString(";")
+      }.mkString("\n")).mkString("")
     }
   }
 
